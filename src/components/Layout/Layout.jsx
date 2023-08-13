b767b5c9ad0bc6dd21912705a2db00d087c9d187
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-08-13 16:13:43 +0300

    first commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+import css from "./Layout.module.css";
+
+export const Layout = ({ children }) => {
+  return <main className={css.container}>{children}</main>;
+};
